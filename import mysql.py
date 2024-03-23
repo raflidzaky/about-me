63d922ef861eb13a02ef51ba565ff3205eed0841
Muhammad Dzaky Rafliansyah <104545005+raflidzaky@users.noreply.github.com>
2024-03-23 23:06:09 +0700

    Update learning to make data pipeline

---
+++ import mysql.py
@@ -0,0 +1,22 @@
+import mysql.connector as connection
+import pandas as pd
+
+# Connect to the MySQL connector
+db = connection.connect(host='127.0.0.1', 
+                        database='bank', 
+                        user='llamaik', 
+                        passwd='radixomniumroma1453',
+                        auth_plugin='mysql_native_password',
+                        )
+
+def get_the_data(database):
+    try:
+        sql = 'SELECT * FROM account'
+        df = pd.read_sql(sql, con=database)
+        df.to_excel('bank_account.xlsx', 
+                     index=False)
+        print('Success!')
+    except Exception as e:
+        print(f'Error!: {e}')
+
+get_the_data(database=db)
